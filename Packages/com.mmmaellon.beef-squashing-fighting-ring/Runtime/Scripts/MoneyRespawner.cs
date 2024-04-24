195fc68718ccfb4caca1dcbbce34d07cebc8418c
cameronmcnz <mmmaellon@gmail.com>
2024-04-23 18:58:57 -0700

    Added button to respawn the money stacks

---
+++ Packages/com.mmmaellon.beef-squashing-fighting-ring/Runtime/Scripts/MoneyRespawner.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+
+namespace MMMaellon.BeefSquashingFightingRing
+{
+    public class MoneyRespawner : UdonSharpBehaviour
+    {
+        public SmartObjectSync[] moneyStacks;
+        public void Respawn()
+        {
+            foreach (SmartObjectSync stack in moneyStacks)
+            {
+                if (stack)
+                {
+                    stack.Respawn();
+                }
+            }
+        }
+    }
+
+}
